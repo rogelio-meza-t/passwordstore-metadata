711ddcde07be2d573d5b0f3064bbd83aee120ef4
Rogelio Meza <rogelio.meza.t@gmail.com>
2019-10-29 01:04:32 -0300

    remove blank line when variable isn't present

--- metadata.bash
+++ metadata.bash
@@ -12,25 +12,18 @@ ENTRY="$1"
 for i in "$@"
 do
     case $i in
-        --username=*) USERNAME="username: ${i#*=}" ; shift ;;
-        --email=*) EMAIL="email: ${i#*=}" ; shift ;;
-        --url=*) URL="URL: ${i#*=}" ; shift ;;
-        --oauth2=*) OAUTH2="OAuth2: ${i#*=}" ; shift ;;
-        --multifactor=*) MFA="MFA: ${i#*=}" ; shift ;;
-        --updated=*) UPDATED="updated: ${i#*=}" ; shift ;;
-        --cycle=*) CYCLE="cycle: ${i#*=}" ; shift ;;
+        --username=*) USERNAME=$'\n'"username: ${i#*=}" ; shift ;;
+        --email=*) EMAIL=$'\n'"email: ${i#*=}" ; shift ;;
+        --url=*) URL=$'\n'"URL: ${i#*=}" ; shift ;;
+        --oauth2=*) OAUTH2=$'\n'"OAuth2: ${i#*=}" ; shift ;;
+        --multifactor=*) MFA=$'\n'"MFA: ${i#*=}" ; shift ;;
+        --updated=*) UPDATED=$'\n'"updated: ${i#*=}" ; shift ;;
+        --cycle=*) CYCLE=$'\n'"cycle: ${i#*=}" ; shift ;;
     esac
 done
 
 read -d '' template <<_EOF_
-${PASSWORD}
-${USERNAME}
-${EMAIL}
-${URL}
-${OAUTH2}
-${MFA}
-${UPDATED}
-${CYCLE}
+${PASSWORD}${USERNAME}${EMAIL}${URL}${OAUTH2}${MFA}${UPDATED}${CYCLE}
 _EOF_
 
 echo "${template}" | pass insert -m "${ENTRY}"
